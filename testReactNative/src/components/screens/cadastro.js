89ebc807ee30702c5abf5fb7d59be7d722ad1810
WMrMW <marciofelipe.mfd@gmail.com>
2022-11-23 18:00:05 -0300

    Adicionanto estilo a algumas telas

--- testReactNative/src/components/screens/cadastro.js
+++ testReactNative/src/components/screens/cadastro.js
@@ -1,66 +1,100 @@
 import React, { useState } from "react";
 import { StatusBar } from "react-native";
-import { Text, TextInput, View, StyleSheet, Image, TouchableOpacity } from "react-native" ;
+import { Text, TextInput, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { useNavigation } from '@react-navigation/native';
 
+import * as Animatable from 'react-native-animatable';
 
 
-export default function Cadastro(){
+
+
+export default function Cadastro() {
 
     const [email, setEmail] = useState(null);
     const [senha, setSenha] = useState(null);
     const [nome, setNome] = useState(null);
+    const [dataNasc, setDataNasc] = useState(null);
 
     const navigationimc = useNavigation();
-    
-   return (
-    <View  styles = {styles.container}>
 
-        <Text style= {styles.textBemVindo}>Por favor digite os campos solicitados abaixo e realize seu cadastro!</Text>
-        <TextInput placeholder="Digite seu nome" style={styles.textInput} onChangeText={text=>setNome(text)}/>
-        <TextInput placeholder="Digite seu email" style={styles.textInput} onChangeText={text=>setEmail(text)}/>
-        <TextInput secureTextEntry ={true} placeholder="Digite sua senha" style={styles.textInput} onChangeText={text=>setSenha(text)}/>
-    
-        <TouchableOpacity style={styles.btnCadastro} onPress={() => navigationimc.navigate('Login')}>
-            <Text style={styles.textbtncadastro} >Cadastrar</Text>
-        </TouchableOpacity>
-    </View>
-   )
+    return (
+        <View styles={styles.container}>
+            <Animatable.View>
+                <View style={styles.titleArea}>
+                    <Text style={styles.textBemVindo}>Crie seu cadastro!</Text>
+                </View>
+                <View style={styles.formArea}>
+                    <Text style={styles.textCamp}>Nome</Text>
+                    <TextInput style={styles.textInput} onChangeText={(text) => setNome(text)} />
+                    <Text style={styles.textCamp}>Email</Text>
+                    <TextInput style={styles.textInput} onChangeText={(text) => setEmail(text)} />
+                    <Text style={styles.textCamp}>Data de Nascimento</Text>
+                    <TextInput style={styles.textInput} onChangeText={(text) => setDataNasc(text)} />
+                    <Text style={styles.textCamp}>Senha</Text>
+                    <TextInput secureTextEntry={true} style={styles.textInput} onChangeText={(text) => setSenha(text)} />
+
+                </View>
+                <View style={styles.btnArea}>
+                    <TouchableOpacity style={styles.btnCadastro} onPress={() => navigationimc.navigate('Login')}>
+                        <Text style={styles.textbtncadastro} >Cadastrar</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </Animatable.View>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#8bfaff',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 20,
+        flex: 1,
+        backgroundColor: '#8bfaff',
     },
-    textBemVindo:{
-       fontSize: 28,
-       fontWeight: 'bold',
-       color: 'black',
-       alignItems: 'center',
-       justifyContent: 'center',
-       padding: 20,
-       paddingTop:35,
+    titleArea: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#8bfaff',
+        padding: 15,
     },
-    textInput:{
-        width: '100%',
-        height: 40,
+    textBemVindo: {
+        fontSize: 25,
+        color: '#1b065e',
+        padding: 50,
+        fontWeight: 'bold',
+    },
+    textCamp: {
+        fontSize: 18,
+        color: '#1b065e',
+        paddingTop: 10,
+        fontWeight: 'bold',
+        paddingLeft:15
+    },
+    formArea: {
+        backgroundColor: '#8bfaff',
+        alignItems:'center',
+    },
+    btnArea: {
+        backgroundColor: '#8bfaff',
+        alignItems:'center',
+        justifyContent:'center',
+        padding:40,
+    },
+    textInput: {
+        width: '75%',
+        height: 35,
         backgroundColor: '#FFF',
         borderRadius: 20,
-        paddingLeft: 10,
-        marginBottom:10,
+        marginLeft: 15,
+        marginTop:15
     },
-    btnCadastro:{
-        width: '100%',
+    btnCadastro: {
+        width: 120,
         height: 40,
-        backgroundColor:'#7b42f5',
+        backgroundColor: '#1b065e',
         borderRadius: 20,
         justifyContent: 'center',
     },
-    textbtncadastro:{
+    textbtncadastro: {
         textAlign: 'center',
         color: "#FFF",
     },
--- testReactNative/src/components/screens/home.js
+++ testReactNative/src/components/screens/home.js
@@ -4,41 +4,49 @@ import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 export default function Welcome() {
 
- const navigation = useNavigation();
+  const navigation = useNavigation();
 
-  return(
+  return (
     <View style={styles.container}>
 
-      <TouchableOpacity 
-        style = {styles.containerbuttontext}
-        onPress={ () => navigation.navigate('Login')}
+      <Text style={{ fontSize: 35, fontWeight: 'bold', color: '#1b065e' }}>
+        Seja Bem Vindo!
+      </Text>
+      <Text style={{ fontSize: 35, fontWeight: 'bold', color: '#1b065e' }}>
+        Clique em Acessar para continuar
+      </Text>
+
+      <TouchableOpacity
+        style={styles.containerbuttontext}
+        onPress={() => navigation.navigate('Login')}
       >
-        <Text style = {styles.buttonText}> Acessar </Text>
+        <Text style={styles.buttonText}> Acessar </Text>
       </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
-    backgroundColor: '#FFF',
-  },
-  containerbuttontext:{
-    position: 'absolute',
-    backgroundColor:"red",
-    borderRadius: 50,
-    paddingVertical:8,
-    width: '60%',
-    alignSelf: 'center',
-    bottom: '15%',
+    backgroundColor: '#8bfaff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonText:{
+  containerbuttontext: {
+    position: 'absolute',
+    backgroundColor: '#1b065e',
+    borderRadius: 50,
+    paddingVertical: 8,
+    width: 120,
+    bottom: '15%',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonText: {
     color: '#FFF',
     fontSize: 18,
     fontWeight: 'bold',
   },
-  
+
 })
