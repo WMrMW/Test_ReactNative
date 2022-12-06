fed2428ec45dc0847b620148888490d12d956c76
christianrafael21 <christianrafael102@gmail.com>
2022-12-05 22:12:48 -0300

    settime no cadastro

--- testReactNative/src/components/screens/cadastro.js
+++ testReactNative/src/components/screens/cadastro.js
@@ -52,8 +52,10 @@ export default function Cadastro() {
             setmessageee(ress);
             setTimeout(()=>{
               setmessageee('');
-            },5000) 
-            navigationimc.navigate('Login')
+            },3000) 
+            setTimeout(()=>{
+                navigationimc.navigate('Login')
+              },4000)
         }
        // setmessageee(ress);  
     }
@@ -61,113 +63,115 @@ export default function Cadastro() {
 
     return (
         <KeyboardAvoidingView behavior="position" styles={styles.container} enabled>
-          <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-            <Animatable.View>
-                <View style={styles.titleArea}>
-                    <Text style={styles.textBemVindo}>Crie seu cadastro!</Text>
-                </View>
+         
+                <Animatable.View>
+                    <View style={styles.titleArea}>
+                        <Text style={styles.textBemVindo}>Crie seu cadastro!</Text>
+                    </View>
 
-                <View style={styles.formArea}>
-                    {message && (
-                        <Text style={styles.messageErrorLogin}>{message}</Text>
-                    )}
-                    <Text style={styles.textCamp}>Nome</Text>
+                    <View style={styles.formArea}>
+                        {message && (
+                            <Text style={styles.messageErrorLogin}>{message}</Text>
+                        )}
+                        <Text style={styles.textCamp}>Nome</Text>
+                        <Controller
+                            control={control}
+                            name = "nome"
+                            render={({field: { onChange,onBlur,value} }) => (
+                                <TextInput 
+                                style={[styles.textInput,
+                                {borderWidth: errors.name && 1,
+                                borderColor: errors.name && '#ff375b'}]}
+                                onChangeText={text=>setNome(text)} 
+                                onBlur={onBlur}
+                                value ={value}
+                                placeholder= "Digite seu nome"
+                                />
+                            )}
+                        />
+                        {errors.name &&  <Text style = {styles.labelError}>{errors.name?.message}</Text>}
+
+                    <Text style={styles.textCamp}>Email</Text> 
+                        <Controller
+                            control={control}
+                            name = "email"
+                            render={({field: {onChange, onBlur,value} }) => (
+                                <TextInput 
+                                style={[styles.textInput,
+                                {borderWidth: errors.email && 1,
+                                borderColor: errors.email && '#ff375b'}]}
+                                onChangeText={text=>setEmail(text)} 
+                                onBlur={onBlur}
+                                value ={value}
+                                keyboardType = "email-address"
+                                placeholder = "Digite seu email"
+                                />
+                            )}
+                        />
+
+                    {errors.email &&  <Text style = {styles.labelError}>{errors.email?.message}</Text>}
+
+                        <Text style={styles.textCamp}>Data de Nascimento</Text>
+                        
+                        <Controller
+                            control={control}
+                            name = "dataNasc"
+                            render={({field: { onChange,onBlur,value} }) => (
+                            <TextInput 
+                            style={[styles.textInput,
+                                {borderWidth: errors.dataNasc && 1,
+                                    borderColor: errors.dataNasc && '#ff375b'}]}
+                                onChangeText={text=>setDataNasc(text)}
+                                onBlur={onBlur}
+                                value ={value}
+                                keyboardType = "numbers-and-punctuation"
+                                placeholder= "Digite sua data de nascimento"
+                                />
+                            )}
+                        />
+                        
+                        {errors.dataNasc &&  <Text style = {styles.labelError}>{errors.dataNasc?.message}</Text>}
+
+                        <Text style={styles.textCamp}>Senha</Text>
                     <Controller
-                        control={control}
-                        name = "nome"
-                        render={({field: { onChange,onBlur,value} }) => (
-                            <TextInput 
-                            style={[styles.textInput,
-                            {borderWidth: errors.name && 1,
-                            borderColor: errors.name && '#ff375b'}]}
-                            onChangeText={text=>setNome(text)} 
-                            onBlur={onBlur}
-                            value ={value}
-                            placeholder= "Digite seu nome"
-                            />
-                        )}
-                    />
-                    {errors.name &&  <Text style = {styles.labelError}>{errors.name?.message}</Text>}
-
-                  <Text style={styles.textCamp}>Email</Text> 
-                    <Controller
-                        control={control}
-                        name = "email"
-                        render={({field: {onChange, onBlur,value} }) => (
-                            <TextInput 
-                            style={[styles.textInput,
-                            {borderWidth: errors.email && 1,
-                            borderColor: errors.email && '#ff375b'}]}
-                            onChangeText={text=>setEmail(text)} 
-                            onBlur={onBlur}
-                            value ={value}
-                            placeholder = "Digite seu email"
-                            />
-                        )}
-                    />
-
-                   {errors.email &&  <Text style = {styles.labelError}>{errors.email?.message}</Text>}
-
-                     <Text style={styles.textCamp}>Data de Nascimento</Text>
-                     
-                      <Controller
-                        control={control}
-                        name = "dataNasc"
-                        render={({field: { onChange,onBlur,value} }) => (
-                          <TextInput 
-                          style={[styles.textInput,
-                            {borderWidth: errors.dataNasc && 1,
-                                borderColor: errors.dataNasc && '#ff375b'}]}
-                            keyboardType = {"numeric"} 
-                            onChangeText={text=>setDataNasc(text)}
-                            onBlur={onBlur}
-                            value ={value}
-                            placeholder= "Digite sua data de nascimento"
-                            />
-                        )}
-                    />
-                    
-                    {errors.dataNasc &&  <Text style = {styles.labelError}>{errors.dataNasc?.message}</Text>}
-
-                    <Text style={styles.textCamp}>Senha</Text>
-                   <Controller
-                        control={control}
-                        name = "senha"
-                        render={({field: {onChange, onBlur,value} }) => (
-                            <TextInput 
-                            secureTextEntry={true} 
-                            style={[styles.textInput,
-                            {borderWidth: errors.senha && 1,
-                            borderColor: errors.senha && '#ff375b'}]}
-                            onChangeText={text=>setSenha(text)}
-                            onBlur={onBlur}
-                            value ={value}
-                            placeholder= "Digite sua senha"
-                            />
-                        )}
-                    />
-                    {errors.senha &&  <Text style = {styles.labelError}>{errors.senha?.message}</Text>}
+                            control={control}
+                            name = "senha"
+                            render={({field: {onChange, onBlur,value} }) => (
+                                <TextInput 
+                                secureTextEntry={true} 
+                                style={[styles.textInput,
+                                {borderWidth: errors.senha && 1,
+                                borderColor: errors.senha && '#ff375b'}]}
+                                onChangeText={text=>setSenha(text)}
+                                onBlur={onBlur}
+                                value ={value}
+                                placeholder= "Digite sua senha"
+                                />
+                            )}
+                        />
+                        {errors.senha &&  <Text style = {styles.labelError}>{errors.senha?.message}</Text>}
 
 
-                </View>
-                <View style={styles.btnArea}>
-                    <TouchableOpacity style={styles.btnCadastro} onPress={fnCadastrar}>
-                        <Text style={styles.textbtncadastro}>Cadastrar</Text>
+                    </View>
+                    <View style={styles.btnArea}>
+                        <TouchableOpacity style={styles.btnCadastro} onPress={fnCadastrar}>
+                            <Text style={styles.textbtncadastro}>Cadastrar</Text>
+                        </TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.buttonregister}
+                    onPress={() => { navigationimc.navigate('Login') }}
+                    >
+                    <Text style={styles.registerText}>Já possui uma conta ? Entre</Text>
                     </TouchableOpacity>
-                <TouchableOpacity
-                   style={styles.buttonregister}
-                  onPress={() => { navigationimc.navigate('Login') }}
-                >
-                  <Text style={styles.registerText}>Já possui uma conta ? Entre</Text>
-                 </TouchableOpacity>
-                </View>
+                    </View>
 
-            </Animatable.View>
-        </TouchableWithoutFeedback>
+                </Animatable.View>
+            
         </KeyboardAvoidingView>
     )
 }
-
+// <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+// </TouchableWithoutFeedback>
 const styles = StyleSheet.create({
     container: {
         flex: 1,
